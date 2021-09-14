24b8b4e0f96d8c65a19a046c058e65da51cd34b7
Phu Nguyen <phulnguyen7@gmail.com>
2021-09-14 17:26:29 -0500

    call REST API to retrieve products in the database

--- Frontend/src/app/display-product/display-product.component.ts
+++ Frontend/src/app/display-product/display-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../classes/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-display-product',
@@ -11,9 +12,16 @@ export class DisplayProductComponent implements OnInit {
 
   products: Array<Product> = [];
 
-  constructor(public router: Router) { }
+  constructor(
+    public router: Router,
+    public productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.productService.getAllProducts().subscribe(
+      result => {this.products = result},
+      error => console.log(error)
+    )
   }
 
   addProductToCart(product: Product) {
