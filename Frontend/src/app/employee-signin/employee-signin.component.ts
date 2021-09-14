a4fec15961fb83a1a536a9b0624d8501acb460a9
Anthony Ho <AnthonyHo0728@gmail.com>
2021-09-14 11:22:45 -0700

    basic signin complete + routing

--- Frontend/src/app/employee-signin/employee-signin.component.ts
+++ Frontend/src/app/employee-signin/employee-signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ControlContainer, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-signin',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeSigninComponent implements OnInit {
 
+  loginRef = new FormGroup({
+    email:new FormControl(),
+    password:new FormControl()
+  })
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  checkLogin(){
+    let data = this.loginRef.value;
+    console.log(data);
+  }
 }
