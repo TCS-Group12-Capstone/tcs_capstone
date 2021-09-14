4f1a30d29354e11afcaffae00eb40561068752e1
Anthony Ho <AnthonyHo0728@gmail.com>
2021-09-14 09:02:59 -0700

    Basic update form

---
+++ Frontend/src/app/edit-user-profile/edit-user-profile.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ControlContainer, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-user-profile',
+  templateUrl: './edit-user-profile.component.html',
+  styleUrls: ['./edit-user-profile.component.css']
+})
+export class EditUserProfileComponent implements OnInit {
+  userProfileRef = new FormGroup({
+    address:new FormControl(),
+    phone:new FormControl(),
+    email:new FormControl(),
+    password:new FormControl()
+  })
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updateUserInfo(){
+    let data = this.userProfileRef.value;
+    console.log(data);
+
+    this.userProfileRef.reset();
+  }
+
+}
