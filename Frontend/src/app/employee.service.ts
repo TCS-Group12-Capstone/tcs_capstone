130d13a5796fd96575c398b5a8991f836b9891da
Phu Nguyen <phulnguyen7@gmail.com>
2021-09-15 17:53:44 -0500

    restore the missing methods of employee service

--- Frontend/src/app/employee.service.ts
+++ Frontend/src/app/employee.service.ts
@@ -11,46 +11,58 @@ export class EmployeeService {
 
   constructor(public http: HttpClient) { }
 
-  empAccountCreate(addEmployee:Employee):Observable<any>{
-    return this.http.post("http://localhost:1020/api/employee/addEmployee",addEmployee,
-    {responseType:'text'});
+  empAccountCreate(addEmployee: Employee): Observable<any> {
+    return this.http.post("http://localhost:1020/api/employee/addEmployee", addEmployee,
+      { responseType: 'text' });
   }
-  
+
   sentRequests(request: Request): Observable<any> {
     return this.http.post("http://localhost:1020/api/requests/send-requests", request);
   }
 
-  empAccountDelete(empEmail:String):Observable<any>{
+  empAccountDelete(empEmail: String): Observable<any> {
     console.log(empEmail)
     //empEmail = JSON.stringify(empEmail)
-    return this.http.delete("http://localhost:1020/api/employee/deleteEmployee/"+empEmail
-    ,{responseType:'text'});
+    return this.http.delete("http://localhost:1020/api/employee/deleteEmployee/" + empEmail
+      , { responseType: 'text' });
   }
 
-  userSignUp(userInfo:Employee):Observable<any>{
-    return this.http.post("http://localhost:1020/api/user/signUp",userInfo,
+  userSignUp(userInfo: Employee): Observable<any> {
+    return this.http.post("http://localhost:1020/api/user/signUp", userInfo,
+      { responseType: 'text' });
+  }
+
+  userSignIn(userInfo: Employee): Observable<any> {
+    return this.http.post("http://localhost:1020/api/user/signIn", userInfo,
+      { responseType: 'text' });
+  }
+
+  updateProfile(user: any): Observable<any> {
+    return this.http.put("http://localhost:1020/api/employee/update", user);
+  }
+
+  getProfile(): Observable<any> {
+    return this.http.post("http://localhost:1020/api/employee/profile", {});
+  }
+
+  empSignIn(userInfo:Employee):Observable<any>{
+    return this.http.post("http://localhost:1020/api/user/empSignIn",userInfo,
     {responseType:'text'});
   }
 
-  userSignIn(userInfo:Employee):Observable<any>{
-    return this.http.post("http://localhost:1020/api/user/signIn",userInfo,
-    {responseType:'text'});
-  }
-
-  getDailyReport():Observable<any>{
+  getDailyReport(): Observable<any> {
     return this.http.get("http://localhost:1020/api/reports/getDailyReports");
-   }
+  }
 
-   getMonthlyReport():Observable<any>{
+  getMonthlyReport(): Observable<any> {
     return this.http.get("http://localhost:1020/api/reports/getMonthlyReports");
-   }
+  }
 
-   getWeeklyReport():Observable<any>{
+  getWeeklyReport(): Observable<any> {
     return this.http.get("http://localhost:1020/api/reports/getWeeklyReports");
-   }
+  }
 
-   getItemReport(itemId:Number):Observable<any>{
-    return this.http.post("http://localhost:1020/api/reports/getProductReports",itemId);
-   }
-
+  getItemReport(itemId: Number): Observable<any> {
+    return this.http.post("http://localhost:1020/api/reports/getProductReports", itemId);
+  }
 }
