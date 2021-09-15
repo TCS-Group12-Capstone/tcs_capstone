863da34922693af4f55ea0e4a06165686dce0cfa
Phu Nguyen <phulnguyen7@gmail.com>
2021-09-14 19:22:40 -0500

    commenting

--- Backend/controller/cart.controller.js
+++ Backend/controller/cart.controller.js
@@ -4,9 +4,9 @@ let addCart = (request, response) => {
     let product = request.body;
 
     cartModel.updateOne(
-        {$and : [{userId : product.userId}, {productId : product.productId}]}, 
-        {$inc : {quantity : product.quantity}},
-        {upsert : true},
+        {$and : [{userId : product.userId}, {productId : product.productId}]},  // filter
+        {$inc : {quantity : product.quantity}}, // increment the quantity
+        {upsert : true},    // allow to insert a new document on the first add
         (err, result) => {
             if (!err) {
                 response.json("Added to cart");
