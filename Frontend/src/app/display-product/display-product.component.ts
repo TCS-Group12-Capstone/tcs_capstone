78d59a0f2c62aebe791aafd9b719de33e355c908
Phu Nguyen <phulnguyen7@gmail.com>
2021-09-14 19:18:13 -0500

    styling for display-product, enable add to cart

--- Frontend/src/app/display-product/display-product.component.ts
+++ Frontend/src/app/display-product/display-product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Cart } from '../classes/cart';
 import { Product } from '../classes/product';
+import { CartService } from '../services/cart.service';
 import { ProductService } from '../services/product.service';
 
 @Component({
@@ -14,7 +16,8 @@ export class DisplayProductComponent implements OnInit {
 
   constructor(
     public router: Router,
-    public productService: ProductService
+    public productService: ProductService,
+    public cartService: CartService
   ) { }
 
   ngOnInit(): void {
@@ -25,7 +28,12 @@ export class DisplayProductComponent implements OnInit {
   }
 
   addProductToCart(product: Product) {
+    let selectedProduct = new Cart("123", product._id, 1);
 
+    this.cartService.addCart(selectedProduct).subscribe(
+      result => console.log(result),
+      error => console.log(error)
+    )
   }
 
   goToCart() {
