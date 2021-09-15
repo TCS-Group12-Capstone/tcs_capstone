531479b44e97982152af15acc46764dc7d97b73a
BIsrat2017 <bisratmendefera@gmail.com>
2021-09-15 10:26:21 -0700

    back end reports

--- Backend/app.js
+++ Backend/app.js
@@ -29,8 +29,12 @@ app.use("/api/user",accountRouter);
 
 
 //http://localhost:1020/api/reports/getAllReports
+//http://localhost:1020/api/reports/getDailyReports
+//http://localhost:1020/api/reports/getWeeklyReports
+//http://localhost:1020/api/reports/getMonthlyReports
+//http://localhost:1020/api/reports/getProductReports
+//http://localhost:1020/api/reports/getCustomerReports
 app.use("/api/reports",reportsRouter);
-app.listen(1020,()=>console.log("Server running on port number 1020"))
 
 
 //http://localhost:1020/api/requests/send-requests
--- Backend/controller/reports.controller.js
+++ Backend/controller/reports.controller.js
@@ -1,7 +1,7 @@
 let reportsModel = require ("../model/report.model")
 
 
-let getAllreports = (request,response)=> {
+let getAllReports = (request,response)=> {
 
     reportsModel.find({},(err,data) => {
         if(!err){
@@ -13,7 +13,71 @@ let getAllreports = (request,response)=> {
 
 }
 
-module.exports = { getAllreports }
+
+let getDailyReport = (request,response)=> {
+    
+    reportsModel.find({},(err,data) => {
+        if(!err){
+            response.json(data);
+        }else {
+             response.json(err);   
+        }
+    })
+
+}
+
+
+let getMonthlyReports = (request,response)=> {
+
+    reportsModel.find({},(err,data) => {
+        if(!err){
+            response.json(data);
+        }else {
+             response.json(err);   
+        }
+    })
+
+}
+
+let getWeeklyReports = (request,response)=> {
+
+    reportsModel.find({},(err,data) => {
+        if(!err){
+            response.json(data);
+        }else {
+             response.json(err);   
+        }
+    })
+
+}
+
+
+let getCustomerReports = (request,response)=> {
+
+    reportsModel.find({},(err,data) => {
+        if(!err){
+            response.json(data);
+        }else {
+             response.json(err);   
+        }
+    })
+
+}
+
+
+let getProductReports = (request,response)=> {
+
+    reportsModel.find({},(err,data) => {
+        if(!err){
+            response.json(data);
+        }else {
+             response.json(err);   
+        }
+    })
+
+}
+
+module.exports = { getAllReports,getDailyReport,getMonthlyReports,getWeeklyReports,getCustomerReports,getProductReports}
 
 
 
