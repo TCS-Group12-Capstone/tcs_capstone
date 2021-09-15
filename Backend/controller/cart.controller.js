ba8061750fe30e4e37e184ad4e42aa900054f895
Phu Nguyen <phulnguyen7@gmail.com>
2021-09-14 19:20:09 -0500

    fix parsing issue

--- Backend/controller/cart.controller.js
+++ Backend/controller/cart.controller.js
@@ -11,7 +11,7 @@ let addCart = (request, response) => {
             if (!err) {
                 response.json("Added to cart");
             } else {
-                console.send(err); 
+                console.json(err); 
             }
     })
 }
